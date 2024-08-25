a4066ee7260a0495e955cf81c24f6300e571828a
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-08-25 21:37:42 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
   "http://localhost:3500",
-  "https://node-expressapi-9qd9.onrender.com/",
-  "https://node-expressapi-9qd9.onrender.com/auth"
+  "https://node-expressapi-9qd9.onrender.com",
 ];
 
 module.exports = allowedOrigins;
